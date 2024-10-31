a03898d91c8d86c3349e76cf8b604ba29bb68531
Umar <Umar234123>
2024-10-31 22:01:20 +0500

    Redesign

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const animationContainer = document.createElement('div');
-    animationContainer.id = 'lottie-container';
-    document.body.appendChild(animationContainer);
+jQuery(document).ready(function($) {
+    // Trigger on button click
+    $('.learn-more-btn').click(function(event) {
+        event.preventDefault();
+        
+        
+        var additionalInfo = $(this).siblings('.additional-info');
+        
+        
+        $('.additional-info').not(additionalInfo).slideUp();
+        
+        
+        additionalInfo.slideToggle();
+    });
+});
 
-    
-    const animData = {
-        container: animationContainer,
+$(document).ready(function() {
+    let animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-container'),
         renderer: 'svg',
         loop: true,
         autoplay: true,
         path: 'assets/animation.json' 
-    };
+    });
 
     
-    const anim = lottie.loadAnimation(animData);
+    function resizeLottie() {
+        const container = $('#lottie-container');
+        container.css('height', container.width() * 0.75); 
+    }
+
+    $(window).on('resize', resizeLottie);
+    resizeLottie(); 
 });
-
-/*
-jQuery(document).ready(function($) {
-    $('.learn-more-btn').click(function(event) {
-        event.preventDefault();
-        var additionalInfo = $(this).siblings('.additional-info');
-        
-        $('.additional-info').not(additionalInfo).slideUp();
-        additionalInfo.slideToggle();
-    });
-
-    $('.learn-more-btn').mouseleave(function() {
-        var additionalInfo = $(this).siblings('.additional-info');
-        additionalInfo.slideUp();
-    });
-});
-*/
-jQuery(document).ready(function($) {
-    // Trigger on button click
-    $('.learn-more-btn').click(function(event) {
-        event.preventDefault(); // Prevent default behavior (e.g., link navigation)
-        
-        // Get the additional info section that's next to the clicked button
-        var additionalInfo = $(this).siblings('.additional-info');
-        
-        // Close any other open .additional-info sections
-        $('.additional-info').not(additionalInfo).slideUp();
-        
-        // Toggle the visibility of the current additional-info section
-        additionalInfo.slideToggle();
-    });
-});
-
-
-  
